c838d1a6c4a1704b0d9931de3490970594c62342
toanos <toan.ha1896@gmail.com>
2021-10-19 11:59:18 -0700

    implement GET request for SpoonacularAPI.

---
+++ app/src/main/java/com/example/spoonacularapisandbox/SpoonacularAPiInterface.kt
@@ -0,0 +1,11 @@
+package com.example.spoonacularapisandbox
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface SpoonacularAPiInterface {
+
+    // This is a hard coded implementation
+    @GET(value = "food/ingredients/search?query=banana&number=5&apiKey=9f7cdafec5da49c3bad4c81f2e08713c")
+    fun getSearchIngredients(): Call<List<Ingredient>>
+}
